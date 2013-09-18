f0aa5f641c5f7362674010e8fe99e1ea8dcd4d9e
Siarhei_Klimuts <Siarhei_Klimuts@EPBYGROW0110.grodno.epam.com>
2013-09-18 14:50:01 +0300

    Removed old code

--- src/main/java/com/epam/trn/web/grid/Grid.java
+++ src/main/java/com/epam/trn/web/grid/Grid.java
@@ -5,19 +5,7 @@ import java.util.List;
 public interface Grid<T> {
 
 	public Integer getRecords();//total records
-
-	//public void setRecords(Integer records);
-
 	public Integer getTotal();//total pages
-
-	//public void setTotal(Integer total);
-
 	public Integer getPage();//current page
-
-	//public void setPage(Integer page);
-
 	public List<T> getRows();//rows for current page
-
-	//public void setRows(List<T> rows);
-
 }
